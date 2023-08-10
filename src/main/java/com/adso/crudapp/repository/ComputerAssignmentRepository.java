ee62b42f1f1faf3f04bac4931fd0112a81dbc0ab
Andresv309 <andresv309@gmail.com>
2023-08-10 14:05:10 -0500

    Add repository files for jpa functionalities

--- src/main/java/com/adso/crudapp/repository/ComputerAssignmentRepository.java
+++ src/main/java/com/adso/crudapp/repository/ComputerAssignmentRepository.java
@@ -3,9 +3,9 @@ package com.adso.crudapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.adso.crudapp.model.Computer;
+import com.adso.crudapp.model.ComputerAssignment;
 
 @Repository
-public interface ComputerRepository extends JpaRepository<Computer, Long> {
+public interface ComputerAssignmentRepository extends JpaRepository<ComputerAssignment, Long>{
 
 }
--- src/main/java/com/adso/crudapp/repository/ComputerStockRepository.java
+++ src/main/java/com/adso/crudapp/repository/ComputerStockRepository.java
@@ -3,9 +3,9 @@ package com.adso.crudapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.adso.crudapp.model.Computer;
+import com.adso.crudapp.model.ComputerStock;
 
 @Repository
-public interface ComputerRepository extends JpaRepository<Computer, Long> {
+public interface ComputerStockRepository extends JpaRepository<ComputerStock, Long>{
 
 }
