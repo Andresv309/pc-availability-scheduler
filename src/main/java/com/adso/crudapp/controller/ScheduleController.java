d6e3ae1a4f2681321aae135da0cf5e25ece01235
Andresv309 <andresv309@gmail.com>
2023-08-10 14:02:53 -0500

    Remove unused files

--- src/main/java/com/adso/crudapp/controller/ScheduleController.java
+++
@@ -1,5 +0,0 @@
-package com.adso.crudapp.controller;
-
-public class ScheduleController {
-
-}
--- src/main/java/com/adso/crudapp/repository/ScheduleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.adso.crudapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.adso.crudapp.model.Schedule;
-
-public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-
-}
