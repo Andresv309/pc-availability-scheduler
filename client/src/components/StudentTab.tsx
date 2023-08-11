25569d06f1bae61fa15474cd84e0f5eb78b03e96
Vanessahdz12 <131272063+Vanessahdz12@users.noreply.github.com>
2023-08-10 23:44:37 -0500

    Add some table test code

--- client/src/components/StudentTab.tsx
+++ client/src/components/StudentTab.tsx
@@ -1,10 +1,24 @@
-import { TabPanel, Table, TableBody, TableCell, TableHead, TableHeaderCell, TableRow } from "@tremor/react";
-import { useEffect, useState } from "react";
+// import { TabPanel, Table, TableBody, TableCell, TableHead, TableHeaderCell, TableRow } from "@tremor/react";
+import { useEffect, useRef, useState } from "react";
 import { Student, fetchStudents } from "../api/fetch";
 
+import { HotTable, HotColumn } from '@handsontable/react';
+import { registerAllModules } from 'handsontable/registry';
+import 'handsontable/dist/handsontable.full.min.css';
+import { TabPanel } from "@tremor/react";
+
+// import {
+//   drawCheckboxInRowHeaders,
+//   addClassesToRows,
+//   changeCheckboxCell,
+//   alignHeaders
+// } from "../hooks/tableHooks";
+
+registerAllModules()
 
 export function StudentTab () {
   const [students, setStudents] = useState<Student[]>([])
+  const hotTableComponentRef = useRef(null);
 
   useEffect(() => {
     const fetchStudentData = async () => {
@@ -16,8 +30,85 @@ export function StudentTab () {
   }, [])
 
   return (
+
+
+
     <TabPanel >
-      <Table>
+      {/* <HotTable
+        ref={hotTableComponentRef}
+        data={students}
+        rowHeaders={true}
+        // colHeaders={[
+        //   "No.",
+        //   "Nombre",
+        //   "Tipo de documento",
+        //   "Numero de documento",
+        //   "Jornada"
+        // ]}
+        columns={[
+          { data: 'id', title: "ID", readOnly: true },
+          { data: 'name', title: "Nombre"  },
+          { data: 'cardIdType', title: "Tipo de documento", type: 'dropdown', source: ['C.C.', 'T.I.'] },
+          { data: 'cardIdNumber', title: "Numero de documento"  },
+          { data: 'session', title: "Jornada"}
+        ]}
+        
+        height="auto"
+        width="100%"
+        stretchH="all"
+        
+        dropdownMenu={true}
+        hiddenColumns={{
+          indicators: true
+        }}
+        contextMenu={true}
+        multiColumnSorting={true}
+        filters={true}
+        customBorders={true}
+        manualRowMove={true}
+
+        className="customFilterButtonExample1"
+        licenseKey="non-commercial-and-evaluation"
+      /> */}
+
+<HotTable
+      data={students}
+      height={450}
+      colWidths={[140, 126, 192, 100, 100]}
+      colHeaders={[
+        "No.",
+        "Nombre",
+        "Tipo de documento",
+        "Numero de documento",
+        "Jornada"
+      ]}
+      dropdownMenu={true}
+      hiddenColumns={{
+        indicators: true
+      }}
+      contextMenu={true}
+      multiColumnSorting={true}
+      filters={true}
+      rowHeaders={true}
+      // afterGetColHeader={alignHeaders}
+      // beforeRenderer={addClassesToRows}
+      // afterGetRowHeader={drawCheckboxInRowHeaders}
+      // afterOnCellMouseDown={changeCheckboxCell}
+      manualRowMove={true}
+      licenseKey="non-commercial-and-evaluation"
+    >
+      <HotColumn data={1} />
+      <HotColumn data={2} />
+      <HotColumn data={3} />
+      <HotColumn data={4} type="date" allowInvalid={false} />
+      <HotColumn data={5} />
+      <HotColumn data={6} type="checkbox" className="htCenter" />
+      <HotColumn data={7} type="numeric" />
+    </HotTable>
+
+
+
+      {/* <Table>
         <TableHead>
           <TableRow>
             <TableHeaderCell>Nombre</TableHeaderCell>
@@ -36,7 +127,7 @@ export function StudentTab () {
             </TableRow>
           ))}
         </TableBody>
-      </Table>
+      </Table> */}
     </TabPanel>
   )
 }
