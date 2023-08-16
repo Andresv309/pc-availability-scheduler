ba675e7c1efeec5e7ebbd5d43f7c02462ca1c744
Andresv309 <andresv309@gmail.com>
2023-08-15 20:06:11 -0500

    Comment entity code for later removal

--- server/src/main/java/com/adso/crudapp/repository/ComputerAssignmentRepository.java
+++ server/src/main/java/com/adso/crudapp/repository/ComputerAssignmentRepository.java
@@ -1,11 +1,11 @@
 package com.adso.crudapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.adso.crudapp.model.ComputerAssignment;
-
-@Repository
-public interface ComputerAssignmentRepository extends JpaRepository<ComputerAssignment, Long>{
-
-}
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import com.adso.crudapp.model.ComputerAssignment;
+//
+//@Repository
+//public interface ComputerAssignmentRepository extends JpaRepository<ComputerAssignment, Long>{
+//
+//}
